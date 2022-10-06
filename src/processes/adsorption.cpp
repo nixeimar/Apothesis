197dc8d5ddf6d687d5a23896a86533e554a27e88
Nikolaos (Nikos) Cheimarios <nixeimar@localhost.localdomain>
2022-10-06 16:29:19 +0300

    Fixed compilation error due to refactoring.

--- src/processes/adsorption.cpp
+++ src/processes/adsorption.cpp
@@ -14,7 +14,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //============================================================================
-#include "adsorption_simple_cubic.h"
+#include "adsorption.h"
 
 namespace MicroProcesses
 {
--- src/processes/desorption.cpp
+++ src/processes/desorption.cpp
@@ -14,7 +14,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //============================================================================
-#include "desorption_simple_cubic.h"
+#include "desorption.h"
 
 namespace MicroProcesses
 {
