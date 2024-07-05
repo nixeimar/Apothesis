baf16a74e0712bbdfe4345b2814d25bf18410df1
despknkd <des.kanakidou@gmail.com>
2024-07-05 14:37:53 +0300

    removal of apothesis instance from creation

--- src/lattice/abstract_lattice.h
+++ src/lattice/abstract_lattice.h
@@ -17,8 +17,10 @@ class AbstractLattice
     /// Destructor
     virtual ~AbstractLattice(){}
 
-    /// Pure virtual method for creating a process.
-    virtual Lattice* create(Apothesis* apothesis)= 0; 
+    /// Pure virtual method for creating a lattice.
+    virtual Lattice* create()= 0; 
+    // removal of apothesis instance from creation
+    // virtual Lattice* create(Apothesis* apothesis)= 0; 
   };
 
 #endif // ABSTRACT_LATTICE_H
