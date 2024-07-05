4cc2ae188c38e52161382e1f5837f5a92789e180
despknkd <des.kanakidou@gmail.com>
2024-07-05 14:38:40 +0300

    factory_lattice getTable addition

--- src/lattice/factory_lattice.h
+++ src/lattice/factory_lattice.h
@@ -1,13 +1,31 @@
+//============================================================================
+//    Apothesis: A kinetic Monte Calro (KMC) code for deposition processes.
+//    Copyright (C) 2019  Nikolaos (Nikos) Cheimarios
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//============================================================================
+
 #ifndef FACTORY_LATTICE_H
 #define FACTORY_LATTICE_H
 
 #include <string>
+#include <map>
 
-#include "register.h"
+#include "register_lattice.h"
 
 class Lattice;
 class AbstractLattice;
-class Apothesis;
+// class Apothesis;
 
 using namespace std;
 
@@ -24,16 +42,18 @@ class FactoryLattice
     static void registerThis( const string& , AbstractLattice* );
 
     /// Create the lattice
-    static Lattice* createLattice( const string&, Apothesis* );
+    static Lattice* createLattice( const string& );
 
-//should we include the getTable for the lattice?
+private:
+    /// The factory map
+    static map< string, AbstractLattice* >& getTable();
 
-}
+};
 
 #define REGISTER_LATTICE( __NAME__ ) \
-    private: static const Register< __NAME__> creator;
+    private: static const RegisterLattice< __NAME__> creator;
 
 #define REGISTER_LATTICE_IMPL( __NAME__) \
-    const Register< __NAME__ > __NAME__::creator(#__NAME__);
+    const RegisterLattice< __NAME__ > __NAME__::creator(#__NAME__);
 
 #endif // FACTORY_LATTICE_H
