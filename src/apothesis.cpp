c1457353665f7dc1dd8cef519b8cf63bbacf15f0
Nikolaos Cheimarios <nixeimar@MacBook-Pro-Nikolaos.local>
2025-02-22 17:55:36 +0200

    Fixed compilation issue on cluster.

--- src/apothesis.cpp
+++ src/apothesis.cpp
@@ -43,7 +43,7 @@
 #include <algorithm>
 
 using namespace MicroProcesses;
-namespace fs = std::filesystem;
+using namespace std;
 
 //using namespace Utils;
 
@@ -71,17 +71,17 @@ Apothesis::~Apothesis()
 void Apothesis::mf_createWorkingDir(const string& dirName ){
     try {
         // Check if the directory already exists
-        if (fs::exists(dirName)) {
+        if (filesystem::exists(dirName)) {
             std::cout << "Directory already exists: " << dirName << std::endl;
         } else {
             // Create the directory
-            if (fs::create_directory(dirName)) {
+            if (filesystem::create_directory(dirName)) {
                 std::cout << "Directory created successfully: " << dirName << std::endl;
             } else {
                 std::cerr << "Failed to create directory: " << dirName << std::endl;
             }
         }
-    } catch (const fs::filesystem_error& e) {
+    } catch (const filesystem::filesystem_error& e) {
         std::cerr << "Filesystem error: " << e.what() << std::endl;
     }
 }
